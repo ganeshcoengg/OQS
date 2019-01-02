d9e63fe239e6240ad377b5219dadf63f9ebb2387
Ganesh Vahinde <ganesh.vahinde@officebox.co.in>
2019-01-02 18:22:31 +0530

    EmployeeController.cs chnged

--- OQS/APIcontroller/EmployeeController.cs
+++ OQS/APIcontroller/EmployeeController.cs
@@ -11,34 +11,27 @@ namespace OQS.APIcontroller
     public class EmployeeController : ApiController
     {
         // GET api/employee
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
         // GET api/employee/5
-        public Employee Get(string EmployeeCode)
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployee()
         {
             Employee employee = new Employee();
-            if (EmployeeCode == "EMP001")
-            {
+            List<Employee> employees = new List<Employee>();
+          
                 employee.CompanyCode = "BIZ001";
                 employee.CompnayName = "Bizense";
                 employee.DepartmentCode = "DevOpps001";
                 employee.DepartmentName = "Devlopment";
                 employee.EmployeeCode = "EMP001";
                 employee.EmployeeName = "Ganesh Vahinde";
-            }
-            else
-            {
-                employee.CompanyCode = "BIZ001";
-                employee.CompnayName = "Bizense";
-                employee.DepartmentCode = "DevOpps001";
-                employee.DepartmentName = "Devlopment";
-                employee.EmployeeCode = "EMP002";
-                employee.EmployeeName = "Sachin Vahinde";
-            }
-            return employee;
+
+            employees.Add(employee);
+            return employees;
         }
 
         // POST api/employee
