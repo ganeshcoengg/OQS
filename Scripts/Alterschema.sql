c990a351a1b0d9cbd2632ff7091df43f08532b08
Ganesh Vahinde <ganeshcoengg@gmail.com>
2020-01-01 23:51:29 +0530

    Alterschema.sql added

---
+++ Scripts/Alterschema.sql
@@ -0,0 +1,8 @@
+ALTER TABLE 
+	student
+Add 
+	FirstName varchar(250),
+	LastName varchar(250),
+	MiddleName varchar(250),
+	dob date;
+
--- Scripts/CreateTables.sql
+++ Scripts/CreateTables.sql
@@ -3,9 +3,9 @@
 CREATE table student(
 	studentId varchar(250) primary key not null,
 	StudentName varchar(250) not null,
-	studentCode varchar(250) not null,
-	ClassId varchar(250),
-	CollegeId varchar(250),
+	studentCode varchar(250) not null,		-- ganIT202001
+	ClassId varchar(250),					-- IT001
+	CollegeId varchar(250),					-- SSGMCECOL
 	email varchar(250),
 	number varchar(250),
 	AddressId varchar(250)
